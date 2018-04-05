c1e9908c80c46394a505f4bc663bcd0fedce2143
rkett <rkett@uoguelph.ca>
2018-04-05 17:11:58 -0400

    Updated js; removed debugging information.

--- js/custom.js
+++ js/custom.js
@@ -1,30 +1,31 @@
+/* 
+* Variables
+*/
+
+var index = 1; 
+
+/*
+* Initialize the window.
+* Set's opacity of #timeline-fade-1 to 100%
+* simultaenously hides it, and then fades it in.
+*/
 
 $(window).ready(function() {
-    console.log("Window Ready.");
     $("#timeline-fade-1").css("opacity","100");
     $("#timeline-fade-1").css("display","none");
     $("#timeline-fade-1").fadeIn(1000);
-
 });
 
-var index = 1;
+/* 
+* jQuery scroll function. 
+* When scrolling is initiated, alter current indexed
+* #timeline-fade -> opacity: 100% -> display: none -> fadeIn().
+*/
 $(window).scroll(function() {
-    
-    console.log(index);
-
     if (Math.floor($("#timeline-fade-"+index).offset().top) < $(this).scrollTop()){
         index = index + 1;
         $("#timeline-fade-"+index).css("opacity","100");
         $("#timeline-fade-"+index).css("display", "none");
         $("#timeline-fade-"+index).fadeIn(1000);
-        console.log("INDEX"+index);
     };
-
-    console.log($(this).scrollTop());
-    console.log(Math.floor($("#timeline-fade-1").offset().top));
-    console.log(Math.floor($("#timeline-fade-1").offset().top) == $(this).scrollTop());
 });
-
-/*560
-custom.js:13 560.796875
-custom.js:15 false*/

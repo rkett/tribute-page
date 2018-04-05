eb6b29b07281794f8976bd7d21c68441ebaea4ab
rkett <rkett@uoguelph.ca>
2018-04-05 16:57:21 -0400

    Added rough timeline animation.

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,30 @@
 
 $(window).ready(function() {
-	console.log("Window Ready.");
-	$(".timeline-fade").show();
+    console.log("Window Ready.");
+    $("#timeline-fade-1").css("opacity","100");
+    $("#timeline-fade-1").css("display","none");
+    $("#timeline-fade-1").fadeIn(1000);
+
 });
 
-$(document).ready(function() {
-	console.log("Document Ready");
+var index = 1;
+$(window).scroll(function() {
+    
+    console.log(index);
+
+    if (Math.floor($("#timeline-fade-"+index).offset().top) < $(this).scrollTop()){
+        index = index + 1;
+        $("#timeline-fade-"+index).css("opacity","100");
+        $("#timeline-fade-"+index).css("display", "none");
+        $("#timeline-fade-"+index).fadeIn(1000);
+        console.log("INDEX"+index);
+    };
+
+    console.log($(this).scrollTop());
+    console.log(Math.floor($("#timeline-fade-1").offset().top));
+    console.log(Math.floor($("#timeline-fade-1").offset().top) == $(this).scrollTop());
 });
+
+/*560
+custom.js:13 560.796875
+custom.js:15 false*/

73412496918dcd9f92b68ef7866887a377039c1c
rkett <rkett@uoguelph.ca>
2018-04-10 22:58:35 -0400

    Update to custom.js, cleaned code.

--- js/custom.js
+++ js/custom.js
@@ -1,88 +1,11 @@
-/* 
-* Variables
-*/
-
-
-
 /*
 * Initialize the window.
 * Set's opacity of #timeline-fade-1 to 100%
 * simultaenously hides it, and then fades it in.
 */
+$(window).ready(function() {});
 
-$(window).ready(function() {
-    /*$("#timeline-fade-1").css("opacity","1");
-    $("#timeline-fade-1").css("display","none");
-    $("#timeline-fade-1").fadeIn(1000);*/
-});
-
-
-
-
-/*
-var cat = 0;
-
-console.log(current_height[550]);
-console.log(dict);
-
-$(window).scroll(function() {
-    console.log(scroll_pos);
-    scroll_pos = $(this).scrollTop();
-    cat = scroll_pos - $(this).scrollTop();
-    console.log(cat-1+"cat");
-
-    if ((Math.floor($("#timeline-fade-"+index).offset().top) < $(this).scrollTop()) && ($(this).scrollTop() < Math.floor($("#timeline-fade-"+(index+1)).offset().top))) {
-        $("#timeline-fade-"+index).css("opacity","1");
-        $("#timeline-fade-"+index).css("display", "none");
-        $("#timeline-fade-"+index).fadeIn(1000);
-        index = index + 1;
-    };
-});*/
-
-/*
-* jQuery scroll function. 
-* When scrolling is initiated, alter current indexed
-* #timeline-fade -> opacity: 100% -> display: none -> fadeIn().
-*/
-
-/*$(window).scroll(function() {
-    var scrollbottom = Math.abs($(document).height() - $(window).height() - $(window).scrollTop());
-    var wd = $(window).scrollTop(); //$(window).height() - $(window).scrollTop();
-
-    console.log("WD:"+wd);
-    console.log(scrollbottom);
-    console.log($(window).scrollTop());
-
-    if (Math.floor($("#timeline-fade-"+index).offset().top) < ($(window).scrollTop())) {
-        $("#timeline-fade-"+(index+1)).css("opacity","1");
-        $("#timeline-fade-"+(index+1)).css("display", "none");
-        $("#timeline-fade-"+(index+1)).fadeIn(1000);
-        index = index + 1;
-        //console.log("Index IN if #1 :: "+index);
-    };
-});*/
-
-/*console.log("DOCUMENT HEIGHT"+ $(document).height());
-console.log("WINDOW HEIGHT"+ $(window).height());
-
-$(window).scroll(function() {
-    if ((Math.floor($("#timeline-fade-"+index).offset().top) < $(this).scrollTop()) && ($(this).scrollTop() < Math.floor($("#timeline-fade-"+(index+1)).offset().top))) {
-        $("#timeline-fade-"+index).css("opacity","1");
-        $("#timeline-fade-"+index).css("display", "none");
-        $("#timeline-fade-"+index).fadeIn(1000);
-        index = index + 1;
-
-    };
-    console.log("WINDOW HEIGHT"+ $(window).height());
-    console.log(index);
-    console.log(Math.floor($("#timeline-fade-"+(11)).offset().top));
-    console.log($(window).height());
-    console.log(Math.floor($("#timeline-fade-"+(11)).offset().top) == $(this).height());
-    if (Math.floor($("#timeline-fade-"+(11)).offset().top) == $(this).height()) {
-        console.log("AT MAX");
-    };
-});*/
-
+/* Variables declared and initalized. */
 var element_top         = 0; // Top of current element.
 var element_bot         = 0; // Bottom of current element.
 var window_top          = 0; // Top of window.
@@ -91,35 +14,46 @@ var exit                = 0; // For the while loop.
 var index               = 1; // For scrolling down.
 var total_timeline_fade = 1; // Length of total timeline elements.
 
-
+/* Acquire the total number of timeline-fade elements we have. */
 while(exit == 0) {
     if ($("#timeline-fade-"+total_timeline_fade).length) {
-       console.log(total_timeline_fade);
+        console.log(total_timeline_fade); // XXX
         total_timeline_fade = total_timeline_fade + 1
     } else {
         exit = 1;
     }
 };
 
+/*
+* NOTE: <!DOCTYPE html> must be present for $(document) and $(window) to operate correctly.
+* 
+* A check is first made to ensure that we are not on an index greater than the total
+* number of timeline-fade elements. If this passes, we set the next increment of 
+* each given position (See above init.)
+* 
+* Upon any scroll we check if our timeline-fade elements are in the viewport, 
+* if they are display them. The display works as such: Original CSS Opacity: 0 
+* (this acts as a "virtual placeholder" of our element (keeps height)) ->
+* set opacity: 1 -> display: none this removes our placeholder but sets it to
+* none (which allows the fadeIn() function to operate) -> fadeIn(time:1000ms).
+*/
 $(window).scroll(function() {
-    /*console.log("WINDOW HEIGHT: "+$(this).height()); // Window Height.
-    console.log("DOC HEIGHT: "+$(document).height()); // Document Height.
-    console.log("SCROLLTOP: "+$(this).scrollTop()); // Window Scroll Bar Top Height.
-    console.log("FANCY: "+($(this).scrollTop() + $(this).height()));
-    console.log(window.innerHeight);*/
-
     if (index < total_timeline_fade) {
         element_top = $("#timeline-fade-"+index).offset().top;
         element_bot = element_top + $("#timeline-fade-"+index).outerHeight();
         window_top = $(this).scrollTop();
         window_bot = window_top + $(this).height();
+        console.log("ELE TOP"+element_bot); // XXX
+        console.log("ELE BOT"+element_top); // XXX
+        console.log("WIN TOP"+window_top); // XXX
+        console.log("WIN BOT"+window_bot); // XXX
     };
 
+    /* Logic courtesy of https://stackoverflow.com/questions/20791374/jquery-check-if-element-is-visible-in-viewport?lq=1 */
     if ((element_top > window_top && element_bot < window_bot)) {
         $("#timeline-fade-"+index).css("opacity","1");
         $("#timeline-fade-"+index).css("display", "none");
         $("#timeline-fade-"+index).fadeIn(1000);
         index = index + 1;
-        //console.log(index);
     };
 });
